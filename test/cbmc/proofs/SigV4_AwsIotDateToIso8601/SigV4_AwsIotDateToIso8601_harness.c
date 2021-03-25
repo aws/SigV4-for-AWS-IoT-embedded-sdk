ab28f38d1035b8caf7430c4c4c262b33cbe7a7cd
Oscar Abrina <abrinao@amazon.com>
2021-03-25 10:21:55 -0700

    Add sample proof

---
+++ test/cbmc/proofs/SigV4_AwsIotDateToIso8601/SigV4_AwsIotDateToIso8601_harness.c
@@ -0,0 +1,25 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/*
+ * Insert copyright notice
+ */
+
+/**
+ * @file SigV4_AwsIotDateToIso8601_harness.c
+ * @brief Implements the proof harness for SigV4_AwsIotDateToIso8601 function.
+ */
+
+/*
+ * Insert project header files that
+ *   - include the declaration of the function
+ *   - include the types needed to declare function arguments
+ */
+
+void harness()
+{
+
+  /* Insert argument declarations */
+
+  SigV4_AwsIotDateToIso8601( /* Insert arguments */ );
+}
