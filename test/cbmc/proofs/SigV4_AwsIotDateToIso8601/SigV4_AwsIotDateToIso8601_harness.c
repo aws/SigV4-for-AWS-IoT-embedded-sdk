be74c2e1f841d8b54742adc8c2c2e91e297d76b7
Oscar Abrina <abrinao@amazon.com>
2021-04-27 12:48:00 -0700

    Implement CBMC proof for SigV4_AwsIotDateToIso8601

--- test/cbmc/proofs/SigV4_AwsIotDateToIso8601/SigV4_AwsIotDateToIso8601_harness.c
+++ test/cbmc/proofs/SigV4_AwsIotDateToIso8601/SigV4_AwsIotDateToIso8601_harness.c
@@ -1,8 +1,23 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
 /*
- * Insert copyright notice
+ * SigV4 Utility Library v1.0.0
+ * Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 /**
@@ -10,16 +25,21 @@
  * @brief Implements the proof harness for SigV4_AwsIotDateToIso8601 function.
  */
 
-/*
- * Insert project header files that
- *   - include the declaration of the function
- *   - include the types needed to declare function arguments
- */
+/* Include paths for public enums, structures, and macros. */
+#include "sigv4.h"
 
 void harness()
 {
+    char * pInputDate;
+    size_t dateLen;
+    char * pDateISO8601;
+    size_t dateISO8601Len;
 
-  /* Insert argument declarations */
+    __CPROVER_assume( dateLen < CBMC_MAX_OBJECT_SIZE );
+    pInputDate = malloc( dateLen );
 
-  SigV4_AwsIotDateToIso8601( /* Insert arguments */ );
+    __CPROVER_assume( dateISO8601Len < CBMC_MAX_OBJECT_SIZE );
+    pDateISO8601 = malloc( dateISO8601Len );
+
+    SigV4_AwsIotDateToIso8601( pInputDate, dateLen, pDateISO8601, dateISO8601Len );
 }
