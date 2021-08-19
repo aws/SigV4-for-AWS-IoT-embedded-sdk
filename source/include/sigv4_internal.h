096db006e0430b57df80b894359cb3e159fc1974
Shivangi <66447522+gshvang@users.noreply.github.com>
2021-08-19 15:13:56 -0700

    Update source/include/sigv4_internal.h
    Co-authored-by: Oscar Michael Abrina <abrinao@amazon.com>

--- source/include/sigv4_internal.h
+++ source/include/sigv4_internal.h
@@ -205,7 +205,7 @@ typedef struct CanonicalContext
     uint8_t pBufProcessing[ SIGV4_PROCESSING_BUFFER_LENGTH ];       /**< Internal calculation buffer used during canonicalization. */
     char * pBufCur;                                                 /**< pBufProcessing cursor. */
     size_t bufRemaining;                                            /**< pBufProcessing value used during internal calculation. */
-    const char * pHashPayloadLoc;                                   /**< Pointer used to store the location of HTTP request hashed payload. */
+    const char * pHashPayloadLoc;                                   /**< Pointer used to store the location of hashed HTTP request payload. */
     size_t hashPayloadLen;                                          /**< Length of HTTP request hashed payload. */
 } CanonicalContext_t;
 
